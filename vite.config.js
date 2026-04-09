cb2f217be966321aa10e89df4d3133af8809acf2
amarqueze <alanmarquez@outlook.com>
2026-04-08 21:29:08 -0500

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// La configuración de Vite define el directorio base y aplica el plugin de React.
-// El valor base es "/" porque el blog se desplegará en la raíz de https://tuusuario.github.io.
-
 export default defineConfig({
   base: '/',
   plugins: [react()],
